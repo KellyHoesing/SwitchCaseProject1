0bfd9bddc62c0d839dbdfb32950ffcbf687b1a9a
Kelly <kelly.hoesing@gmail.com>
2020-08-25 14:30:14 -0500

    First draft of switch case project

---
+++ SwitchCaseProject1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SwitchCaseProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your favorite classs?");
+            string userInput = Console.ReadLine();
+            switch(userInput.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("You're a logical person");
+                    break;
+                case "english":
+                    Console.WriteLine("You have a poet's soul");
+                    break;
+                case "science":
+                    Console.WriteLine("You love to experiment");
+                    break;
+                case "art":
+                    Console.WriteLine("You have keen observation skills");
+                    break;
+                case "gym":
+                    Console.WriteLine("You're staying fit!");
+                    break;
+                default:
+                    Console.WriteLine("That's certainly interesting");
+                    break;
+            }
+        }
+    }
+}
